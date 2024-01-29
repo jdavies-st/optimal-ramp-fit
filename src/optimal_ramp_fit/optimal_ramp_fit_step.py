d7798b135403e9968ccd974edd23b5c30e5800b1
James Davies <jdavies@mpia.de>
2024-01-29 10:15:57 +0100

    Add OptimalRampFitStep

---
+++ src/optimal_ramp_fit/optimal_ramp_fit_step.py
@@ -0,0 +1,9 @@
+from jwst.stpipe import Step
+
+
+class OptimalRampFit(Step):
+    """Optimal Ramp Fitting from Timothy Brandt"""
+    class_alias = "optimal_ramp_fit"
+
+    def process(self, input_data):
+        pass
